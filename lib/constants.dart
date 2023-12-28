acf35f06f000f798909c25312807c034d1a74f09
Shoaib2472 <75031721+Shoaib2472@users.noreply.github.com>
2023-12-28 23:30:35 +0530

    Constants And Dependency added

---
+++ lib/constants.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+var blueColor = Color.fromARGB(255, 139, 171, 227);
+var blackColor = Colors.black;
+customElevatedButton(String text, BuildContext context, Function onTap) {
+  var screenHeight = MediaQuery.of(context).size.height;
+  var screenwidth = MediaQuery.of(context).size.width;
+  return Container(
+    margin: EdgeInsets.symmetric(
+        horizontal: screenwidth / 100, vertical: screenHeight / 72),
+    child: ElevatedButton(
+      onPressed: () {
+        onTap();
+      },
+      child: Text(
+        text,
+        style: TextStyle(
+            color: blackColor,
+            fontWeight: FontWeight.bold,
+            fontSize: screenHeight / 65),
+      ),
+      style: ElevatedButton.styleFrom(
+          minimumSize: Size(50, 40),
+          padding: EdgeInsets.symmetric(horizontal: 5),
+          shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(screenHeight / 76)),
+          backgroundColor: blueColor),
+    ),
+  );
+}
+
+Text cusText(
+  String text,
+  double fontSize, {
+  FontWeight? Boldness,
+  Color? textColor,
+  Color? backgroundColor,
+}) {
+  return Text(
+    text,
+    style: TextStyle(
+        fontSize: fontSize,
+        fontWeight: Boldness,
+        overflow: TextOverflow.ellipsis,
+        color: textColor,
+        backgroundColor: backgroundColor != null ? backgroundColor : null),
+  );
+}
+
+Text smallText(
+  String text, {
+  FontWeight? Boldness,
+  Color? textColor,
+  Color? backgroundColor,
+}) {
+  return cusText(text, 15,
+      textColor: textColor,
+      Boldness: Boldness,
+      backgroundColor: backgroundColor);
+}
+
+Text mediumText(
+  String text, {
+  FontWeight? Boldness,
+  Color? textColor,
+  Color? backgroundColor,
+}) {
+  return cusText(text, 16,
+      textColor: textColor,
+      Boldness: Boldness,
+      backgroundColor: backgroundColor);
+}
